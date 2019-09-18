eefbd3e5fbb0cc9ee758ee5b0699db1a895c82e5
Lucas Gianotti <gianotti.92@gmail.com>
2019-09-18 00:09:21 -0300

    prueba .gitignore

---
+++ Aplicacion/FrbaOfertas/FrbaOfertas/Connection/ConnectionQuery.cs
@@ -0,0 +1,48 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace FrbaOfertas.Connection
+{
+    public class ConnectionQuery
+    {
+        string ConnectionString = "";  
+        SqlConnection con;  
+  
+        public void OpenConection()  
+        {  
+            con = new SqlConnection(ConnectionString);  
+            con.Open();  
+        }  
+  
+  
+        public void CloseConnection()  
+        {  
+            con.Close();  
+        }  
+  
+  
+        public void ExecuteQueries(string Query_)  
+        {  
+            SqlCommand cmd = new SqlCommand(Query_,con);  
+            cmd.ExecuteNonQuery();  
+        }  
+  
+  
+        public SqlDataReader DataReader(string Query_)  
+        {  
+            SqlCommand cmd = new SqlCommand(Query_,con);  
+            SqlDataReader dr = cmd.ExecuteReader();  
+            return dr;  
+        }  
+  
+  
+        public object ShowDataInGridView(string Query_)  
+        {  
+            SqlDataAdapter dr = new SqlDataAdapter(Query_, ConnectionString);  
+            DataSet ds = new DataSet();  
+            dr.Fill(ds);  
+            object dataum = ds.Tables[0];  
+            return dataum;  
+        }  
+    }
+}
