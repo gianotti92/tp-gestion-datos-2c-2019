ab6bc5639b2b9c2d8fee021d78eee0e337a2aaa5
Lucas Gianotti <gianotti.92@gmail.com>
2019-09-18 00:55:33 -0300

    Se agrega pantalla y se corrigen cosas

--- Aplicacion/FrbaOfertas/FrbaOfertas/Form1.Designer.cs
+++ Aplicacion/FrbaOfertas/FrbaOfertas/Form1.Designer.cs
@@ -23,17 +23,59 @@
         #region Código generado por el Diseñador de Windows Forms
 
         /// <summary>
-        /// Método necesario para admitir el Diseñador. No se puede modificar
-        /// el contenido del método con el editor de código.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ingresarBtn = new System.Windows.Forms.Button();
+            this.nameTxt = new System.Windows.Forms.TextBox();
+            this.contrasenaTxt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ingresarBtn
+            // 
+            this.ingresarBtn.Location = new System.Drawing.Point(115, 183);
+            this.ingresarBtn.Name = "ingresarBtn";
+            this.ingresarBtn.Size = new System.Drawing.Size(128, 46);
+            this.ingresarBtn.TabIndex = 0;
+            this.ingresarBtn.Text = "Ingresar";
+            this.ingresarBtn.UseVisualStyleBackColor = true;
+            this.ingresarBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // nameTxt
+            // 
+            this.nameTxt.Location = new System.Drawing.Point(76, 78);
+            this.nameTxt.Name = "nameTxt";
+            this.nameTxt.Size = new System.Drawing.Size(203, 23);
+            this.nameTxt.TabIndex = 1;
+            // 
+            // contrasenaTxt
+            // 
+            this.contrasenaTxt.Location = new System.Drawing.Point(76, 123);
+            this.contrasenaTxt.Name = "contrasenaTxt";
+            this.contrasenaTxt.Size = new System.Drawing.Size(203, 23);
+            this.contrasenaTxt.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 301);
+            this.Controls.Add(this.contrasenaTxt);
+            this.Controls.Add(this.nameTxt);
+            this.Controls.Add(this.ingresarBtn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ingresarBtn;
+        private System.Windows.Forms.TextBox nameTxt;
+        private System.Windows.Forms.TextBox contrasenaTxt;
     }
 }
 
